ed58bf748b27b1586eaf20e0be2b34d66cfae9f4
houseofyair <houseofyair@gmail.com>
2020-08-23 02:16:01 +0000

    gh-pages

---
+++ precache-manifest.53a1fcf00701bf579bc8cc2ff3bd60b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc95bb8890ebece9a261",
+    "url": "/dnd5e-character-sheets/css/app.df10069c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/dnd5e-character-sheets/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6efdc01855a38b3921070f00e90b07a2",
+    "url": "/dnd5e-character-sheets/index.html"
+  },
+  {
+    "revision": "68f4839bdfe8e36f8088",
+    "url": "/dnd5e-character-sheets/js/about.ff5aca64.js"
+  },
+  {
+    "revision": "dc95bb8890ebece9a261",
+    "url": "/dnd5e-character-sheets/js/app.6d67153b.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/dnd5e-character-sheets/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "0105a1dbaa5e41a52afffc8630916cc7",
+    "url": "/dnd5e-character-sheets/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dnd5e-character-sheets/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dnd5e-character-sheets/precache-manifest.53a1fcf00701bf579bc8cc2ff3bd60b4.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "dnd5e-character-sheets"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
